ce4083b30ddefd11f735db2b4fdb6ba68cd1c9d4
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-11-13 11:27:37 -0800

    more updates

--- shell.c
+++ shell.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 
-char* allocate_env_output(char **env_p, char *lineptr, char **user_argv)
+char *allocate_env_output(char **env_p, char *lineptr, char **user_argv)
 {
 	char *env = *env_p;
 	char *env_output = NULL;
@@ -8,12 +8,9 @@ char* allocate_env_output(char **env_p, char *lineptr, char **user_argv)
 	size_t total_length = 0;
 
 	while (env != NULL)
-	{	
-	
+	{
 		env_length = strlen(env);
-
 		env_output = realloc(env_output, total_length + env_length + 2);
-
 		if (env_output == NULL)
 		{
 			perror("Memory allocation error");
@@ -42,72 +39,46 @@ int main(int ac, char **argv)
 	char *prompt = "($) ";
 	char *lineptr = NULL;
 	const char *delim = " \n";
-	char **user_argv = NULL;
-	int token_num = 0;
-	char *token = NULL;
-	int i = 0;
-	char **env_p = environ;
-	char *env_output = NULL;
-	int result;
-
-
-	/* declaring void variables */
+	int result, token_num = 0, i = 0;
+	char *token = NULL, **user_argv = NULL, **env_p = environ, *env_output = NULL;
 	(void)ac;
 	(void)argv;
 
-	/* Create  a loop for shell prompt */
 	while (1)
 	{
-		/* Only print the prompt in interactive mode */
 		if (isatty(STDIN_FILENO))
 		{
 			print_prompt(prompt);
 		} 
-		
-		/* get user input */
 		lineptr = get_command();
-			
-		/* allocate memory for the pointer to the argument variable */
-	/*	user_argv = (char **)malloc(sizeof(char *) * (token_num + 1)); */
-
-		/* split the string/user input into an array of words */
 		user_argv = parse_command(lineptr, delim);
-
-		/* Re-tokenize the input and Store each token in user_argv*/
 		token = strtok(lineptr, delim);
-
-		for (i = 0; i < token_num; i++)
+		while (token != NULL)
 		{
-			if (token != NULL)
-			{
-				user_argv[i] = strdup(token);
-			}
+			user_argv[i] = strdup(token);
 			token = strtok(NULL, delim);
+			i++;
 		}
-
-		if (i >= 0 && i < token_num)
-		{
-			user_argv[i] = NULL;
-		}
-
+		user_argv[i] = NULL;
 		if (user_argv[0] != NULL)
 		{
-			if (strcmp(user_argv[0], "exit") == 0)
+			if (i >= 0 && i < token_num)
 			{
-				free(lineptr);
-				free(user_argv);
-			/*	_print_shell("Exiting shell....\n");*/
-				return (0);
+				if (strcmp(user_argv[0], "exit") == 0)
+				{
+					/*free(lineptr);*/
+					/*free(user_argv);*/
+					return (0);
+				}
 			}
-
 			else if (strcmp(user_argv[0], "env") == 0)
 			{
 				env_output = allocate_env_output(env_p, lineptr, user_argv);
-
 				_print_shell(env_output);
+				free(env_output);
 			}
-				/* execute the command with execve */
-			/*	if (execmd(user_argv) != 0)*/
+			else
+			{
 				result = process_command(user_argv, lineptr, NULL);
 				if (result != 0)
 				{
@@ -115,14 +86,12 @@ int main(int ac, char **argv)
 				}
 			}
 		}
-	
-		/* Free allocated memory */
 		free(lineptr);
-		for (i = 0; i < token_num; i++)
+		for (i = 0; user_argv[i] != NULL; i++)
 		{
 			free(user_argv[i]);
 		}
 		free(user_argv);
-
+	}
 	return (0);
 }
--- shell_parsecommand.c
+++ shell_parsecommand.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
-char** parse_command(char *lineptr, const char *delim)
+char **parse_command(char *lineptr, const char *delim)
 {
+	int i,
 	int token_num = 0;
 	char **user_argv = malloc(sizeof(char *) * BUFFER_SIZE);
-	char *token;
-	char **temp;
+	char *token, **temp;
 
 	if (user_argv == NULL)
 	{
@@ -14,7 +14,6 @@ char** parse_command(char *lineptr, const char *delim)
 
 	if (lineptr == NULL)
 	{
-		/* Handle the error */
 		free(user_argv);
 		return (NULL);
 	}
@@ -22,19 +21,29 @@ char** parse_command(char *lineptr, const char *delim)
 	token = strtok(lineptr, delim);
 	while (token != NULL)
 	{
-		user_argv[token_num] = token;
-		token_num++;
-		if (token_num >= BUFFER_SIZE)
+		if (token_num >= BUFFER_SIZE - 1)
 		{
-			temp = realloc(user_argv, sizeof(char *) * (BUFFER_SIZE + token_num + 1));
+			temp = realloc(user_argv, sizeof(char *) * (BUFFER_SIZE * 2));
 			if (temp == NULL)
 			{
 				free(user_argv);
 				return (NULL);
 			}
 		        user_argv = temp;
+			BUFFER_SIZE *= 2;
 		}
-        	token = strtok(NULL, delim);
+        	user_argv[token_num] = strdup(token);
+		if(user_argv[token_num] == NULL)
+		{
+			for (i = 0; i < token_num; i++)
+			{
+				free(user_argv[i]);
+			}
+			free(user_argv);
+			return (NULL);
+		}
+		token_num++;
+		token = strtok(NULL, delim);
 	}
 	user_argv[token_num] = NULL;
 
